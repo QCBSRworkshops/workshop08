7726ffde40220ff183a24cbf08243ddd9b05d2d3
pedrohbraga <pedrohenrique.pereirabraga@mail.concordia.ca>
2021-07-28 20:38:52 +0000

    Deploying to gh-pages from @ QCBSRworkshops/workshop08@45a1b415462030dff5d5697007f0d30ea60df596 🚀

--- book-fr/01-preparation-pour-l-atelier.R
+++
@@ -1,9 +0,0 @@
-## install.packages("ggplot2")
-## install.packages("mgcv")
-## install.packages("itsadug")
-## 
-## library(ggplot2)
-## library(itsadug)
-## library(itsadug)
-## 
-## source(file.choose()) # use coldiss.R which you have downloaded to your own directory

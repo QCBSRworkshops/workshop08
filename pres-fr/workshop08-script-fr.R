c2a46c03032981aaa89f1a6fca3450fff03a43aa
katherinehebert <katherine.ahebert@gmail.com>
2022-02-11 17:12:48 +0000

    Deploying to gh-pages from @ QCBSRworkshops/workshop08@86cee012ec8a48ddc471e3ff18d8625051e858bb 🚀

--- pres-fr/workshop08-script-fr.R
+++ pres-fr/workshop08-script-fr.R
@@ -1,3 +1,27 @@
+# Function to control the lines shown in code chunk outputs, to avoid overflow
+# note: sometimes cache needs to be set to true in the knitr setup chunk for this to take effect in xaringan::infinite_moon_reader()
+library(knitr)
+hook_output <- knit_hooks$get("output")
+knit_hooks$set(output = function(x, options) {
+   lines <- options$output.lines
+   if (is.null(lines)) {
+     return(hook_output(x, options))  # pass to default hook
+   }
+   x <- unlist(strsplit(x, "\n"))
+   more <- "..."
+   if (length(lines)==1) {        # first n lines
+     if (length(x) > lines) {
+       # truncate the output, but add ....
+       x <- c(head(x, lines), more)
+     }
+   } else {
+     x <- c(more, x[lines], more)
+   }
+   # paste these lines together
+   x <- paste(c(x, ""), collapse = "\n")
+   hook_output(x, options)
+ })
+
 # Standard procedure to check and install packages and their dependencies, if needed.
 
 list.of.packages <- c("ggplot2", "itsadug", "mgcv")

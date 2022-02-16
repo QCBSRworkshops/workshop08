31ec05d86f2acb76d01134e9f381796805c7ac5f
katherinehebert <katherine.ahebert@gmail.com>
2022-02-16 22:36:10 +0000

    Deploying to gh-pages from @ QCBSRworkshops/workshop08@9ccac61bb003b26fc5e5e7f6df35da07065d36c8 🚀

--- book-en/workshop08-script-en.R
+++ book-en/workshop08-script-en.R
@@ -11,37 +11,54 @@ library(itsadug)
 
 ##Section: 02-introduction.R 
 
-library(ggplot2)
-set.seed(10)
-n = 250
-x = runif(n,0,5)
-y_model = 3*x/(1+2*x)
-y_obs = rnorm(n,y_model,0.1)
-data_plot = qplot(x, y_obs) +
-            geom_line(aes(y=y_model)) +
-            theme_bw()
-print(data_plot)
+library(ggplot2, quietly = TRUE)
+library(mgcv, quietly = TRUE)
 
-library(mgcv)
-linear_model = gam(y_obs~x)
-model_summary=summary(linear_model)
-print(model_summary)
-data_plot = data_plot+
-             geom_line(colour="red",
-             aes(y=fitted(linear_model)))
-print(data_plot)
+isit <- read.csv("data/ISIT.csv")
+head(isit)
 
-gam_model = gam(y_obs~s(x))
+isit2 <- subset(isit, Season == 2)
+
+linear_model <- gam(Sources ~ SampleDepth, data = isit2)
+summary(linear_model)
+
+data_plot <- ggplot(data = isit2, aes(y = Sources, x = SampleDepth)) + 
+  geom_point() +
+  geom_line(aes(y = fitted(linear_model)),
+            colour = "red", size = 1.2) + 
+  theme_bw()
+data_plot
+
+gam_model <- gam(Sources ~ s(SampleDepth), data = isit2)
 summary(gam_model)
-data_plot = data_plot +
-     geom_line(colour="blue",aes(y=fitted(gam_model)))
-print(data_plot)
+
+data_plot <- data_plot +
+     geom_line(aes(y = fitted(gam_model)),
+               colour = "blue", size = 1.2)
+data_plot
 
 plot(gam_model)
 
-linear_model = gam(y_obs~x)
-nested_gam_model = gam(y_obs~s(x)+x)
-print(anova(linear_model, nested_gam_model, test="Chisq"))
+linear_model <- gam(Sources ~ SampleDepth, data = isit2)
+smooth_model <- gam(Sources ~ s(SampleDepth), data = isit2)
+AIC(linear_model, smooth_model)
+
+isit1 <- subset(isit, Season == 1)
+
+linear_model_s1 <- gam(Sources ~ SampleDepth, data = isit1)
+smooth_model_s1 <- gam(Sources ~ s(SampleDepth), data = isit1)
+
+ggplot(isit1, aes(x = SampleDepth, y = Sources)) +
+  geom_point() +
+  geom_line(colour = "red", size = 1.2,
+            aes(y = fitted(linear_model_s1))) +
+  geom_line(colour = "blue", size = 1.2,
+            aes(y = fitted(smooth_model_s1))) +
+  theme_bw()
+
+AIC(linear_model_s1, smooth_model_s1)
+
+smooth_model_s1
 
 
 ##Section: 03-smooth-terms.R 
